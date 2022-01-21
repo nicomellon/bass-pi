dae2de32a8d7fce5d4dc3cfbf3541a9b94387102
Nico Mellon <89862838+nicomellon@users.noreply.github.com>
2022-01-21 18:42:55 +0100

    Delete pkg directory

--- pkg/models/bass.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Bass struct {
-	ID               string `json:"id"`
-	Name             string `json:"name"`
-	Manufacturer_ID  string `json:"manufacturer_id"`
-	Launch_year  	 string `json:"launch_year"`
-	Image  	 		 string `json:"image"`
-}
